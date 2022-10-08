c9af266c596f4e7f3fdf00e5092f5255bc6fba09
Matthew <mthunter22@gmail.com>
2022-10-08 18:08:08 -0400

    package updates

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
     rules: [
       {
         test: /\.js?$/,
+        exclude: /node_modules/,
         include: [srcDir],
         loader: require.resolve("esbuild-loader"),
         options: {
